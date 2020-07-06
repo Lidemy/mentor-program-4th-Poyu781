cdcb2fdcd48570ce8dd68a203ebdfdb413be706b
邱泊瑜 <poyuchiu@qiuboyude-MacBook-Pro.local>
2020-07-06 23:12:00 +0800

    week2finish

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,21 @@
 function join(arr, concatStr) {
-  
-}
+    var joinStr = []
+    for(var i =0 ; i < arr.length-1 ;i++){
+        joinStr = joinStr + arr[i] + concatStr
+
+  }
+    return joinStr+arr[arr.length-1]
+} 
 
 function repeat(str, times) {
-  
+    var repeatStr = ""
+    for (var i =1 ;i<= times ; i++){
+        for(var x=0; x<= str.length-1 ; x++){
+            repeatStr = repeatStr + str[x]
+        }
+    }
+    return repeatStr 
 }
 
-console.log(join(['a'], '!'));
+console.log(join(["aaa", "bb", "c", "dddd"], ',,'))
 console.log(repeat('a', 5));
